4bd10eb6cec8d6792e524988b62a0c761c39e069
Ratan <ratan.r.sur@gmail.com>
2016-03-30 20:31:23 -0400

    call_red test function

--- test_red.c
+++ test_red.c
@@ -1,28 +1,41 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 
 #define run_test(fn_name)\
     printf("%s\n", #fn_name);\
     fn_name();
 
-void run_red(char *args, int arglen) {
 
-    char exe[] = "./bin/red";
-    char *command = (char*) calloc(sizeof(exe) + arglen, 1);
-    strcpy(command, exe);
+char *call_red(char *args, int arglen) {
+    char bin[] = "./bin/red ";
+    char *command = (char *) calloc(sizeof(bin) + arglen, sizeof(char));
+    strcpy(command, bin);
     strcat(command, args);
     FILE *fp;
     fp = popen(command, "r");
+    free(command);
 
-    /*if (NULL == fp) {*/
-        /*printf("failure");*/
-        /*exit(1);*/
-    /*}*/
+    if (NULL == fp) {
+        printf("failure");
+        exit(1);
+    }
 
+    printf("\tArgs: %s\n", args);
+    char *lineptr = NULL;
+    size_t len = 0;
+    getline(&lineptr, &len, fp);
     pclose(fp);
+    return lineptr;
+}
+
+void test_bin_exists() {
+    assert(0 == strcmp(call_red("", 0), ""));
 }
 
 int main() {
+    run_test(test_bin_exists);
+    printf("%sPassing.%s\n", "\x1B[32m", "\x1B[0m");
     return 0;
 }
