478dac84d281b22b233e9954521b02fb4f8c1fd3
Ratan <ratan.r.sur@gmail.com>
2016-04-10 16:19:59 -0400

    refactor

--- red.c
+++ red.c
@@ -3,6 +3,25 @@
 #include "utils.c"
 #include <stdint.h>
 
+int64_t sum_from_fileptr(FILE *fp) {
+    int64_t sum = 0;
+
+    while (!feof(fp)) {
+        int64_t current;
+        fscanf(fp, "%lld ", &current);
+        sum += current;
+    }
+
+    return sum;
+}
+
+uint64_t sum_from_filename(char *filename) {
+    FILE *fp = fopen(filename, "r");
+    int64_t sum = sum_from_fileptr(fp);
+    fclose(fp);
+    return sum;
+}
+
 int main(int argc, char **argv) {
     if (argc > 1) {
         int64_t sum = 0;
@@ -11,16 +30,8 @@ int main(int argc, char **argv) {
             char *filename = (char *) calloc(strlen(argv[i]) + 1, 1);
             exit_if_null(filename);
             strcpy(filename, argv[i]);
-            FILE *fp = fopen(filename, "r");
+            sum += sum_from_filename(filename);
             free(filename);
-
-            while (!feof(fp)) {
-                int64_t current;
-                fscanf(fp, "%lld ", &current);
-                sum += current;
-            }
-
-            fclose(fp);
         }
 
         printf("%lld", sum);
