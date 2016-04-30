d87bf976d7e993b4885579db01f9682f1b41b3ff
Ratan <ratan.r.sur@gmail.com>
2016-04-30 10:40:09 -0400

    refactor to use accumulator

--- red.c
+++ red.c
@@ -16,7 +16,7 @@ int64_t sum_from_fileptr(FILE *fp) {
     return sum;
 }
 
-uint64_t sum_from_filename(char *filename) {
+int64_t sum_from_filename(char *filename) {
     FILE *fp = fopen(filename, "r");
     int64_t sum = sum_from_fileptr(fp);
     fclose(fp);
@@ -35,7 +35,7 @@ int64_t prod_from_fileptr(FILE *fp) {
     return prod;
 }
 
-uint64_t prod_from_filename(char *filename) {
+int64_t prod_from_filename(char *filename) {
     FILE *fp = fopen(filename, "r");
     int64_t prod = prod_from_fileptr(fp);
     fclose(fp);
@@ -50,32 +50,37 @@ int main(int argc, char **argv) {
         mode = (char) c;
     }
 
-    if ('s' == mode) {
-        int64_t sum = 0;
+    int64_t accumulator;
 
-        for (int i = optind; i < argc; ++i) {
-            char *filename = (char *) calloc(strlen(argv[i]) + 1, 1);
-            exit_if_null(filename);
-            strcpy(filename, argv[i]);
-            sum += sum_from_filename(filename);
-            free(filename);
-        }
+    switch (mode) {
+    case 's':
+        accumulator = 0;
+        break;
 
-        printf("%lld", sum);
-    } else if ('p' == mode) {
-        int64_t prod = 1;
-
-        for (int i = optind; i < argc; ++i) {
-            char *filename = (char *) calloc(strlen(argv[i]) + 1, 1);
-            exit_if_null(filename);
-            strcpy(filename, argv[i]);
-            prod *= prod_from_filename(filename);
-            free(filename);
-        }
-
-        printf("%lld", prod);
+    case 'p':
+        accumulator = 1;
+        break;
     }
 
+    for (int i = optind; i < argc; ++i) {
+        char *filename = (char *) calloc(strlen(argv[i]) + 1, 1);
+        exit_if_null(filename);
+        strcpy(filename, argv[i]);
+
+        switch (mode) {
+        case 's':
+            accumulator += sum_from_filename(filename);
+            break;
+
+        case 'p':
+            accumulator *= prod_from_filename(filename);
+            break;
+        }
+
+        free(filename);
+    }
+
+    printf("%lld", accumulator);
     printf("\n");
     return 0;
 }
