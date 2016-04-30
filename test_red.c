f497f06f4c1a011041f076a6a7d29b14e570da44
Ratan <ratan.r.sur@gmail.com>
2016-04-30 10:50:34 -0400

    remove unneccessary arglen parameter to call_red

--- test_red.c
+++ test_red.c
@@ -8,13 +8,13 @@
     printf("%s\n", #fn_name);\
     fn_name();
 
-char *call_red(char *args, uint64_t arglen) {
+char *call_red(char *args) {
     char bin[] = "./bin/red ";
-    char *command = (char *) calloc(strlen(bin) + arglen + 1, sizeof(char));
+    char *command = (char *) calloc(strlen(bin) + strlen(args) + 1, sizeof(char));
     strcpy(command, bin);
     strcat(command, args);
     FILE *fp;
-    fp = popen(command, "r");
+    fp = popen(command, "r+");
     free(command);
     exit_if_null(fp);
     char *line = NULL;
@@ -26,70 +26,70 @@ char *call_red(char *args, uint64_t arglen) {
 
 void test_nothing_w_no_args() {
     char args[] = "";
-    char *result = call_red(args, strlen(args));
+    char *result = call_red(args);
     assert(0 == strcmp(result, "0\n"));
     free(result);
 }
 
 void test_one_int() {
     char args[] = "test_files/one_int.txt";
-    char *result = call_red(args, strlen(args));
+    char *result = call_red(args);
     assert(0 == strcmp(result , "1\n"));
     free(result);
 }
 
 void test_two_ints() {
     char args[] = "test_files/two_ints.txt";
-    char *result = call_red(args, strlen(args));
+    char *result = call_red(args);
     assert(0 == strcmp(result , "7\n"));
     free(result);
 }
 
 void test_five_ints() {
     char args[] = "test_files/five_ints.txt";
-    char *result = call_red(args, strlen(args));
+    char *result = call_red(args);
     assert(0 == strcmp(result , "15\n"));
     free(result);
 }
 
 void test_funky_line_breaks() {
     char args[] = "test_files/funky_line_breaks.txt";
-    char *result = call_red(args, strlen(args));
+    char *result = call_red(args);
     assert(0 == strcmp(result , "30\n"));
     free(result);
 }
 
 void test_empty_file() {
     char args[] = "test_files/empty.txt";
-    char *result = call_red(args, strlen(args));
+    char *result = call_red(args);
     assert(0 == strcmp(result , "0\n"));
     free(result);
 }
 
 void test_large_ints() {
     char args[] = "test_files/large_ints.txt";
-    char *result = call_red(args, strlen(args));
+    char *result = call_red(args);
     assert(0 == strcmp(result , "5000000000\n"));
     free(result);
 }
 
 void test_negative_ints() {
     char args[] = "test_files/negative_ints.txt";
-    char *result = call_red(args, strlen(args));
+    char *result = call_red(args);
     assert(0 == strcmp(result , "-9\n"));
     free(result);
 }
 
 void test_two_files() {
     char args[] = "test_files/file1.txt test_files/file2.txt";
-    char *result = call_red(args, strlen(args));
+    char *result = call_red(args);
     assert(0 == strcmp(result , "0\n"));
     free(result);
 }
 
 void test_product_option() {
     char args[] = "-p test_files/five_ints.txt";
-    char *result = call_red(args, strlen(args));
+    char *result = call_red(args);
     assert(0 == strcmp(result , "120\n"));
     free(result);
 }
